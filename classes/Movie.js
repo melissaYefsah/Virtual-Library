17daff0b65a82e5d4dc7e8f314c6baa81b0adb49
Mohamed Rafeek <mohamed.rafeek@mohamed.rafeek-MacBook-Pro>
2024-10-29 15:14:04 -0400

    added movie and music classes

--- classes/Movie.js
+++ classes/Movie.js
@@ -1,4 +1,28 @@
 // import the Media class:
+const Media = require("./Media")
 
+class Movie extends Media{
+    constructor(title,year,genre,director,duration,rating){
+        super(title,year,genre)
+        this.director = director
+        this.duration=duration
+        this.rating=rating
+
+    }
+    static longestMovie(movies){
+        let max = movies[0].duration
+        let longest = movies[0]
+        for(let i = 0; i<movies.length; i++){
+            if(movies[i].duration > longest.duration){
+               longest = movies[i]
+            }
+        }
+
+        return longest
+    }
+    summary(){
+        return `Title: ${this.title}, Director: ${this.director}, Year: ${this.year}, Genre: ${this.genre}, Rating: ${this.rating}`
+    }
+}
 // don't change below
 module.exports = Movie;
--- classes/Music.js
+++ classes/Music.js
@@ -1,6 +1,28 @@
 // import the Media class:
+const Media = require("./Media")
 
 // create your Music class:
+
+class Music extends Media{
+    constructor(title,year,genre,artist,length){
+        super(title,year,genre)
+        this.artist = artist
+        this.length = length
+    }
+    summary(){
+        return `Title: ${this.title}, Artist: ${this.artist}, Year: ${this.year}, Genre: ${this.genre}, Length: ${this.length} seconds`
+    }
+    static shortestAlbum(songs){
+        let min = songs[0]
+
+        for(let i = 0; i<songs.length; i++){
+            if(songs[i].length < min.length){
+                min = songs[i]
+            }
+        }
+        return min
+    }
+}
 
 // don't change below
 module.exports = Music;
